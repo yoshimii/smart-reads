d9f788b5c84f35b99ebf53512cc75ca1c78c3eec
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-11-27 21:05:39 -0600

    organized and styled links

--- src/navigation.js
+++ src/navigation.js
@@ -6,11 +6,16 @@ import Login from './landing-page/login';
 function Navigation() {
 
   return (
+      <>
     <nav className='top-navigation'>
-        <Link to='/'>Home</Link>
-        <Link to='/register'>Join</Link>
-        <Link to='/login'>Sign In</Link>
-
+        <Link to='/' className='logo'><h3>Smart Reads</h3></Link>
+        <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/login'>My Books</Link></li>
+            <li><Link to='/register'>Join</Link></li>
+            <li><Link to='/login'>Sign In</Link></li>
+        </ul>
+        </nav>
         <header>
             <img alt='books in low lit hallway' src='https://i.imgur.com/LsJrzth.jpg'/>
         </header>
@@ -20,7 +25,7 @@ function Navigation() {
         <Route exact path='/' />
         <Route path='/register' component={Register} />
         <Route path='/login' component={Login} />
-    </nav>
+    </>
   );
 }
 
