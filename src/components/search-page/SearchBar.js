71a1d2fe50f6d2e9f27b933985ac0f9b99c53102
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-12 17:42:52 -0600

    Search bar starter

---
+++ src/components/search-page/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchBar.css';
+const SearchBar = () => {
+    return (
+        <form className="search-bar">
+        <p>What's your vibe?</p>
+        <input>
+        </input>
+        <button>Search</button>
+        </form>
+    )
+}
+
+export default SearchBar;
