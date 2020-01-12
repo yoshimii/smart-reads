ac69d3b494dfcc73f86fba230e743ab0b5a11eeb
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-12 17:40:50 -0600

    import search bar component to route

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Register from './components/landing-page/register';
 import Login from './components/landing-page/login';
-import Home from './components/landing-page/Home'
+import Home from './components/landing-page/Home';
+import SearchBar from './components/search-page/SearchBar.js';
 
 const routes = [
         
         <Route exact path='/' render={(props) => <Home {...props} />}/>,
         <Route path='/register' component={Register} />,
         <Route path='/login' component={Login} />,
+        <Route exact path='/' component={SearchBar} />,
     ]
 
 export default routes;
