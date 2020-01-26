33b233e9ce8f0a64d9f75e724e044cfa51f66403
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-26 02:45:25 -0600

    map state to props and card render

--- src/components/search-page/SearchBar.js
+++ src/components/search-page/SearchBar.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
+import ResultCard from './ResultCard.js';
 import './SearchBar.css';   
 import { axiosWithAuth } from '../../utils/axiosWithAuth.js';
+import { connect } from 'react-redux';
 
-const SearchBar = () => {
+const SearchBar = ({dispatch, state}) => {
     const [ searchPhrase, setSearchPhrase ] = useState({ description: '' })
-
+    const [ results, setResults ] = useState([])
+    
     const submitHandler = (e) => {
         e.preventDefault()
+        dispatch({ type: 'REQUEST_BOOKS'})
         console.log(searchPhrase)
         axiosWithAuth().post('https://better-reads-bw.herokuapp.com/api/user/description', searchPhrase)
-            .then( res => console.log(res) )
+            .then( res => {
+                setResults(res.data.books)
+                dispatch({ type: 'RECEIVE_BOOKS' })
+            })
             .then( err => console.log(err) )
-
+        setSearchPhrase({description: ''})
     }
 
     const changeHandler = (e) => {
         setSearchPhrase({description: e.target.value})
     }
-
-    return (
+    console.log(results)
+    return ([
         <form className="search-bar" onSubmit={submitHandler}>
         <p>What's your vibe?</p>
         <input
@@ -29,8 +36,29 @@ const SearchBar = () => {
         placeholder="beach-side spy thriller"
         />
         <button>Search</button>
-        </form>
-    )
+        </form>,
+        <div>
+        {/* {state && <span>...Computing...Beep...Boop</span>} */}
+        {!state && (
+            <>
+            {results.map( book => {
+                console.log(typeof(book.authors))
+                return <ResultCard key={book.isbn} 
+                title={book.title} 
+                authors={book.authors.split(' ').reverse().join(', ')} 
+                rating={book.rating} 
+                isbn={book.isbn}
+                />
+                
+        })}
+            </>
+        )}
+        
+        </div>
+
+    ])
 }
 
-export default SearchBar;
+const mapStateToProps = (state) => ({ state: state.isFetching })
+
+export default connect( mapStateToProps )(SearchBar)
